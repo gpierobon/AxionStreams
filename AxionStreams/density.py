started sampling from star density, fix units and allow multiple orbits

AxionStreams/density.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 
 def rhoR(x,y,z):
     '''
@@ -31,3 +31,19 @@ def stellar_density(x,y,z):
     Units are kpc for input and SolarMasses/pc^3 for output
     '''
     return rhoR(x,y,z)+rhoz1(x,y,z)+rhoz2(x,y,z)
+
+def sample_from_stellar_density(ts,Orbits,j,samplesize=100):
+    '''
+    Sample density values from the calculated stellar density
+    Returns sampled timestamps and density values
+
+    FIX: for now it onl takes a single orbit,
+         there is an issue with time units (has to be dimensionless)
+    '''
+    rho = stellar_density(Orbits[:,0,j],Orbits[:,1,j],Orbits[:,2,j])
+    sorted_indices = np.argsort(ts)
+    sampled_ts = np.random.choice(ts, size=samplesize, replace=False)
+    sampled_indices = np.searchsorted(ts[sorted_indices], sampled_ts)
+    sorted_sampled_indices = sorted_indices[sampled_indices]
+    sampled_rho = rho[sorted_sampled_indices]
+    return sampled_ts,sampled_rho